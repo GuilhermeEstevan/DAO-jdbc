c9159035a1706760fb403cf673fd71295756d8d7
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2024-01-07 13:19:47 -0300

    Dao interfaces

---
+++ src/dao/SellerDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller seller);
+
+    void update(Seller seller);
+
+    void deleteById(int id);
+
+    Seller findById(int id);
+
+    List<Seller> findAll();
+}
