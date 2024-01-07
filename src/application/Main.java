7af67146e0f83a61bd19cf71fbc5d78ece58da42
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2024-01-07 13:09:36 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +11,14 @@ public class Main {
         Department department = new Department(1, "Books");
         System.out.println(department);
 
+        Seller seller = new Seller(
+                21,
+                "Bob",
+                "bob@gmail.com",
+                new Date(),
+                3000,
+                department);
 
+        System.out.println(seller);
     }
 }
